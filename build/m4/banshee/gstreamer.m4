3897cd076add99d0940cbfc0853701a4782b2700
Stephan Sundermann <stephansundermann@gmail.com>
2013-12-20 15:04:11 +0100

    GStreamerSharp: update to work with new 0.99.0 version
    The new 0.99.0 version of this binding targets GStreamer 1.0. This
    has been an accomplishment that took many weeks, starting from the
    Gnome .NET 2013 Hackfest in Vienna.
    
    Contributions from:
     Stephan Sundermann,
     Olivier Dufour,
     Andres G. Aragoneses

--- build/m4/banshee/gstreamer.m4
+++ build/m4/banshee/gstreamer.m4
@@ -1,9 +1,11 @@
 AC_DEFUN([BANSHEE_CHECK_GSTREAMER],
 [
+	GSTREAMER_SHARP_REQUIRED_VERSION=0.99.0
+
 	AC_ARG_ENABLE(gst_sharp, AC_HELP_STRING([--enable-gst-sharp], [Enable Gst# backend]), , enable_gst_sharp="no")
 
 	if test "x$enable_gst_sharp" = "xyes"; then
-		PKG_CHECK_MODULES(GST_SHARP, gstreamer-sharp-0.10)
+		PKG_CHECK_MODULES(GST_SHARP, gstreamer-sharp-1.0 >= $GSTREAMER_SHARP_REQUIRED_VERSION)
 		AC_SUBST(GST_SHARP_LIBS)
 		AM_CONDITIONAL(ENABLE_GST_SHARP, true)
 
--- src/Backends/Banshee.GStreamerSharp/Banshee.GStreamerSharp/Visualization.cs
+++ src/Backends/Banshee.GStreamerSharp/Banshee.GStreamerSharp/Visualization.cs
@@ -1,10 +1,14 @@
 ﻿//
 // Visualization.cs
 //
-// Author:
+// Authors:
 //   olivier dufour <olivier.duff@gmail.com>
+//   Andrés G. Aragoneses <knocte@gmail.com>
+//   Stephan Sundermann <stephansundermann@gmail.com>
 //
 // Copyright (C) 2011 olivier dufour.
+// Copyright (C) 2013 Andrés G. Aragoneses
+// Copyright (C) 2013 Stephan Sundermann
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -31,7 +35,6 @@ using System.Runtime.InteropServices;
 
 using Gst;
 using Gst.Base;
-using Gst.CorePlugins;
 
 using Hyena;
 
@@ -46,16 +49,9 @@ namespace Banshee.GStreamerSharp
         Adapter vis_buffer;
         bool active;
         bool vis_thawing;
-        IntPtr vis_fft;
-        GstFFTF32Complex[] vis_fft_buffer;
+        Gst.FFT.FFTF32 vis_fft;
+        Gst.FFT.FFTF32Complex[] vis_fft_buffer;
         float[] vis_fft_sample_buffer;
-        uint wanted_size;
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct GstFFTF32Complex {
-          public float r;
-          public float i;
-        };
 
         enum FFTWindow
         {
@@ -66,96 +62,78 @@ namespace Banshee.GStreamerSharp
           Blackman
         }
 
-        [DllImport ("libgstfft-0.10.dll")]
-        private static extern IntPtr gst_fft_f32_new (int len, bool inverse);
-
-        [DllImport ("libgstfft-0.10.dll")]
-        private static extern void gst_fft_f32_window (IntPtr self, [MarshalAs (UnmanagedType.LPArray)] float [] timedata, FFTWindow window);
-
-        [DllImport ("libgstfft-0.10.dll")]
-        private static extern void gst_fft_f32_fft (IntPtr self, [MarshalAs (UnmanagedType.LPArray)] float [] timedata, [MarshalAs (UnmanagedType.LPArray, ArraySubType=UnmanagedType.Struct)] GstFFTF32Complex [] freqdata);
-
-        [DllImport ("libgstfft-0.10.dll")]
-        private static extern void gst_fft_f32_free (IntPtr self);
-
-        public Visualization (Bin audiobin, Pad teepad)
+        internal Visualization (PlayerEngine.AudioSinkBin audiobin)
         {
             // The basic pipeline we're constructing is:
             // .audiotee ! queue ! audioresample ! audioconvert ! fakesink
 
             Element converter, resampler;
-            Queue audiosinkqueue;
+            Element audiosinkqueue;
             Pad pad;
 
             vis_buffer = null;
-            vis_fft = gst_fft_f32_new (SLICE_SIZE * 2, false);
-            vis_fft_buffer = new GstFFTF32Complex [SLICE_SIZE + 1];
+            vis_fft = new Gst.FFT.FFTF32 (SLICE_SIZE * 2, false);
+            vis_fft_buffer = new Gst.FFT.FFTF32Complex [SLICE_SIZE + 1];
             vis_fft_sample_buffer = new float [SLICE_SIZE];
             
             // Core elements, if something fails here, it's the end of the world
-            audiosinkqueue = (Queue)ElementFactory.Make ("queue", "vis-queue");
-        
+            audiosinkqueue = ElementFactory.Make ("queue", "vis-queue");
+
             pad = audiosinkqueue.GetStaticPad ("sink");
-            pad.AddEventProbe (new PadEventProbeCallback (EventProbe));
-        
+            pad.AddProbe (PadProbeType.EventDownstream, EventProbe);
+
             resampler = ElementFactory.Make ("audioresample", "vis-resample");
             converter = ElementFactory.Make ("audioconvert", "vis-convert");
-            FakeSink fakesink = ElementFactory.Make ("fakesink", "vis-sink") as FakeSink;
-        
-            // channels * slice size * float size = size of chunks we want
-            wanted_size = (uint)(2 * SLICE_SIZE * sizeof(float));
-        
+            Element fakesink = ElementFactory.Make ("fakesink", "vis-sink");
+
             if (audiosinkqueue == null || resampler == null || converter == null || fakesink == null) {
                 Log.Debug ("Could not construct visualization pipeline, a fundamental element could not be created");
                 return;
             }
-        
+
+            //http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gstreamer-plugins/html/gstreamer-plugins-queue.html#GstQueueLeaky
+            const int GST_QUEUE_LEAK_DOWNSTREAM = 2;
+
             // Keep around the 5 most recent seconds of audio so that when resuming
             // visualization we have something to show right away.
-            audiosinkqueue.Leaky = Queue.LeakyType.Downstream;
-            audiosinkqueue.MaxSizeBuffers = 0;
-            audiosinkqueue.MaxSizeBytes = 0;
-            audiosinkqueue.MaxSizeTime = Clock.Second * 5;
-            
-            fakesink.Handoff += PCMHandoff;
-        
-        
+            audiosinkqueue ["leaky"] = GST_QUEUE_LEAK_DOWNSTREAM;
+            audiosinkqueue ["max-size-buffers"] = 0;
+            audiosinkqueue ["max-size-bytes"] = 0;
+            audiosinkqueue ["max-size-time"] = ((long)Constants.SECOND) * 5L;
+
+            fakesink.Connect ("handoff", PCMHandoff);
+
             // This enables the handoff signal.
-            fakesink.SignalHandoffs = true;
+            fakesink ["signal-handoffs"] = true;
             // Synchronize so we see vis at the same time as we hear it.
-            fakesink.Sync = true;
+            fakesink ["sync"] = true;
             // Drop buffers if they come in too late.  This is mainly used when
             // thawing the vis pipeline.
-            fakesink.MaxLateness = (long)(Clock.Second / 120);
+            fakesink ["max-lateness"] = ((long)Constants.SECOND / 120L);
             // Deliver buffers one frame early.  This allows for rendering
             // time.  (TODO: It would be great to calculate this on-the-fly so
             // we match the rendering time.
-            fakesink.TsOffset = -(long)(Clock.Second / 60);
+            fakesink ["ts-offset"] = -((long)Constants.SECOND / 60L);
             // Don't go to PAUSED when we freeze the pipeline.
-            fakesink.Async = false;
-            
+            fakesink ["async"] = false;
+
             audiobin.Add (audiosinkqueue, resampler, converter, fakesink);
-            
+
             pad = audiosinkqueue.GetStaticPad ("sink");
+            Pad teepad = audiobin.RequestTeePad ();
             teepad.Link (pad);
-            
+
+            teepad.Dispose ();
+            pad.Dispose ();
+
             Element.Link (audiosinkqueue, resampler, converter);
-            
+
             converter.LinkFiltered (fakesink, caps);
-            
+
             vis_buffer = new Adapter ();
             vis_resampler = resampler;
             vis_thawing = false;
             active = false;
-        
-            // Disable the pipeline till we hear otherwise from managed land.
-            Blocked = true;
-        }
-
-        ~Visualization ()
-        {
-            if (vis_fft != IntPtr.Zero)
-                gst_fft_f32_free (vis_fft);
         }
 
         public bool Active
@@ -167,29 +145,36 @@ namespace Banshee.GStreamerSharp
         }
 
         private Caps caps = Caps.FromString (
-            "audio/x-raw-float, " +
+            "audio/x-raw, " +
+            //FIXME: is this correct way to port this? https://github.com/GNOME/banshee/commit/e40923df1bc55129832dff5ca2c782f5040b412f#diff-7c442526ef990528be03ffbca9921ec3R38
+            "format = (string) " + Gst.Audio.GlobalAudio.AudioFormatToString (Gst.Audio.AudioFormat.F32) + ", " +
             "rate = (int) 44100, " +
-            "channels = (int) 2, " +
-            "endianness = (int) BYTE_ORDER, " +
-            "width = (int) 32");
+            "channels = (int) 2");
 
-        private void BlockCallback (Pad pad, bool blocked)
-        {
-            if (!blocked) {
-                // Set thawing mode (discards buffers that are too old from the queue).
-                vis_thawing = true;
-            }
-        }
-        
+        ulong? block_probe = null;
         private bool Blocked
         {
             set {
                 if (vis_resampler == null)
                     return;
+
                 Pad queue_sink = vis_resampler.GetStaticPad ("src");
-                queue_sink.SetBlocked (value, new PadBlockCallback (BlockCallback));
+                if (value) {
+                    if (!block_probe.HasValue) {
+                        block_probe = queue_sink.AddProbe (PadProbeType.Block, (o, i) => PadProbeReturn.Ok);
+                    }
+                } else {
+                    if (block_probe.HasValue) {
+                        queue_sink.RemoveProbe (block_probe.Value);
+                        block_probe = null;
+                    }
+
+                    // Set thawing mode (discards buffers that are too old from the queue).
+                    vis_thawing = true;
+                }
             }
         }
+
         private event VisualizationDataHandler OnDataAvailable = null;
         public event VisualizationDataHandler DataAvailable {
             add {
@@ -215,14 +200,15 @@ namespace Banshee.GStreamerSharp
             }
         }
 
-        private void PCMHandoff (object o, FakeSink.HandoffArgs args)
+        private void PCMHandoff (object o, GLib.SignalArgs args)
         {
-            Gst.Buffer data;
-        
+            byte[] data;
+            uint wanted_size;
+
             if (OnDataAvailable == null) {
                 return;
             }
-        
+
             if (vis_thawing) {
                 // Flush our buffers out.
                 vis_buffer.Clear ();
@@ -231,23 +217,25 @@ namespace Banshee.GStreamerSharp
                 vis_thawing = false;
             }
         
-            Structure structure = args.Buffer.Caps [0];
-            int channels = (int)structure.GetValue ("channels");
+            Caps caps = ((Gst.Pad)args.Args [1]).CurrentCaps;
+
+
+            Structure structure = caps [0];
+            int channels = (int)structure.GetValue ("channels").Val;
         
             wanted_size = (uint)(channels * SLICE_SIZE * sizeof (float));
 
             //TODO see if buffer need a copy or not
             //but copy is no available in gst# ;(
-            vis_buffer.Push (args.Buffer);
+            vis_buffer.Push ((Gst.Buffer)args.Args[0]);
             int i, j;
-            while ((data = vis_buffer.Peek (wanted_size)) != null) {
-                float[] buff = new float[data.Size];
-                Marshal.Copy (data.Data, buff, 0, (int) data.Size);
+            while ((data = vis_buffer.Map ()) != null) {
+                float[] buff = new float[data.Length];
                 float[] deinterlaced = new float [channels * SLICE_SIZE];
                 float[] specbuf = new float [SLICE_SIZE * 2];
 
                 System.Array.Copy (specbuf, vis_fft_sample_buffer, SLICE_SIZE);
-                
+
                 for (i = 0; i < SLICE_SIZE; i++) {
                     float avg = 0.0f;
         
@@ -257,83 +245,72 @@ namespace Banshee.GStreamerSharp
                         deinterlaced[j * SLICE_SIZE + i] = sample;
                         avg += sample;
                     }
-        
+
                     avg /= channels;
                     specbuf[i + SLICE_SIZE] = avg;
                 }
-        
+
                 System.Array.Copy (vis_fft_sample_buffer, 0, specbuf, SLICE_SIZE, SLICE_SIZE);
-        
-                gst_fft_f32_window (vis_fft, specbuf, FFTWindow.Hamming);
-                gst_fft_f32_fft (vis_fft, specbuf, vis_fft_buffer);
-        
+
+                vis_fft.Window (specbuf, Gst.FFT.FFTWindow.Hamming);
+                vis_fft.Fft (specbuf, vis_fft_buffer);
+
                 for (i = 0; i < SLICE_SIZE; i++) {
                     float val;
-        
-                    GstFFTF32Complex cplx = vis_fft_buffer[i];
-        
-                    val = cplx.r * cplx.r + cplx.i * cplx.i;
+
+                    Gst.FFT.FFTF32Complex cplx = vis_fft_buffer[i];
+
+                    val = cplx.R * cplx.R + cplx.I * cplx.I;
                     val /= SLICE_SIZE * SLICE_SIZE;
                     val = (float)(10.0f * System.Math.Log10 ((double)val));
-        
+
                     val = (val + 60.0f) / 60.0f;
                     if (val < 0.0f)
                         val = 0.0f;
-        
+
                     specbuf[i] = val;
                 }
-        
+
                 float [] flat = new float[channels * SLICE_SIZE];
                 System.Array.Copy (deinterlaced, flat, flat.Length);
-        
+
                 float [][] cbd = new float[channels][];
                 for (int k = 0; k < channels; k++) {
                     float [] channel = new float[SLICE_SIZE];
                     System.Array.Copy (flat, k * SLICE_SIZE, channel, 0, SLICE_SIZE);
                     cbd [k] = channel;
                 }
-        
+
                 float [] spec = new float [SLICE_SIZE];
                 System.Array.Copy (specbuf, spec, SLICE_SIZE);
-        
+
                 try {
                     OnDataAvailable (cbd, new float[][] { spec });
                 } catch (System.Exception e) {
                     Log.Exception ("Uncaught exception during visualization data post.", e);
                 }
-        
+
                 vis_buffer.Flush ((uint)wanted_size);
             }
         }
-        
-        bool EventProbe (Pad pad, Event padEvent)
+
+        PadProbeReturn EventProbe (Pad pad, PadProbeInfo info)
         {
+            if (info.Type == PadProbeType.EventDownstream)
+                return PadProbeReturn.Pass;
+
+            var padEvent = info.Event;
             switch (padEvent.Type) {
                 case EventType.FlushStart:
                 case EventType.FlushStop:
                 case EventType.Seek:
-                case EventType.NewSegment:
+                case EventType.Segment:
                 case EventType.CustomDownstream:
                     vis_thawing = true;
                 break;
             }
-        
-            if (active)
-                return true;
-        
-            switch (padEvent.Type) {
-                case EventType.Eos:
-                case EventType.CustomDownstreamOob:
-                    Blocked = false;
-                    break;
-            
-                case EventType.NewSegment:
-                case EventType.CustomDownstream:
-                    Blocked = true;
-                    break;
-            }
-        
-            return true;
+
+            return PadProbeReturn.Pass;
         }
     }
 }
